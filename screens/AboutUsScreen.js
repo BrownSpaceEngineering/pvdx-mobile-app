2344d25b157871c81872104c39bba665e9bac681
Richard Tang <ringtack2002@gmail.com>
2021-03-07 18:03:48 -0500

    Added basic NewsButton; changed AboutUsScreen style a little

--- screens/AboutUsScreen.js
+++ screens/AboutUsScreen.js
@@ -1,20 +1,28 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { Text, View } from 'react-native';
+import NewsButton from '../components/NewsButton';
 
 const AboutUsScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>About us!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Updates</Text>
+      <NewsButton title="Lorem ipsum dolor sit amet." date="1/1/21" url="https://www.google.com" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    justifyContent: 'center', 
+    flex: 1,
+    marginLeft: "5%",
+    // justifyContent: 'center',
+    // alignItems: 'center',
+  },
+  header: {
+    justifyContent: 'center',
     alignItems: 'center',
+    fontSize: 48,
   }
 })
 
