f5c4752e71ee101be1e960a2f7a9975ec76597a4
Sophia <sophiatu217@gmail.com>
2021-12-06 20:29:46 -0500

    Changed DataScreen to use FlatList

--- screens/DataScreen.js
+++ screens/DataScreen.js
@@ -3,11 +3,46 @@ import {
   Text,
   Image,
   StyleSheet,
-  SafeAreaView,
   ScrollView,
+  FlatList,
   View,
 } from "react-native";
 import { Card } from "../components/Card";
+
+const DATA = [
+  {
+    id: "first",
+    content:
+      "Solar Panel Info\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: "second",
+    content: "Battery Info\nLorem ipsum dolor sit amet",
+  },
+  {
+    id: "third",
+    content:
+      "Other things\nLorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    id: "fourth",
+    content: "Even more other things\nLorem ipsum",
+  },
+  {
+    id: "fifth",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    id: "sixth",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+];
+
+const Item = ({ body }) => (
+  <Card width="46%" children={<Text style={styles.body}>{body}</Text>} />
+);
 
 const DataScreen = () => {
   return (
@@ -20,33 +55,13 @@ const DataScreen = () => {
           </Text>
         }
       />
-      <View style={styles.rowContainer}>
-        <Card
-          width="46%"
-          children={
-            <View>
-              <Text style={styles.body}>Solar Panel Info</Text>
-            </View>
-          }
-        />
-        <Card
-          width="46%"
-          children={
-            <View>
-              <Text style={styles.body}>Battery Info</Text>
-              <Text style={styles.body}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Text>
-            </View>
-          }
-        />
-      </View>
+      <FlatList
+        pointerEvents="none"
+        data={DATA}
+        numColumns={2}
+        keyExtractor={(item, index) => item.id}
+        renderItem={(item) => <Item body={item.item.content} />}
+      />
     </ScrollView>
   );
 };
@@ -54,11 +69,6 @@ const DataScreen = () => {
 const styles = StyleSheet.create({
   container: {
     margin: "2%",
-  },
-  rowContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
   },
   column: {
     flex: 1,
